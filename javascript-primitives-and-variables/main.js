095467bdcac65fe35006876b843da6b2327de9dd
Jared Jenvey <jared@exit-12.com>
2022-12-21 22:56:01 +0000

    Completed javascript-primitives-and-variables exercise

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,20 @@
+var fullName = 'Jared Jenvey';
+var isCool = true;
+var totalPets = 2;
+
+console.log('value of fullName: ', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets: ', totalPets);
+console.log('typeOf fullName:', typeof fullName);
+console.log('typeOf isCool:', typeof isCool);
+console.log('typeOf totalPets:', typeof totalPets);
+
+var unknown = null;
+
+console.log('value of unknown: ', unknown);
+console.log('typeOf unknown:', typeof unknown);
+
+var never;
+
+console.log('value of never: ', never);
+console.log('typeOf never:', typeof never);
