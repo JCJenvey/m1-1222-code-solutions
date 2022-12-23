aed4256b3fc92d3114a478d5f20846403ead5ebb
Jared Jenvey <jared@exit-12.com>
2022-12-23 03:26:54 +0000

    Completed javascript-operators-and-expressions exercise

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 10;
+var height = 5;
+var area = width * height;
+console.log('value of area: ', area);
+console.log('typeof area: ', typeof area);
+
+var bill = 21;
+var payment = 15.5;
+var change = bill - payment;
+console.log('value of change: ', change);
+console.log('typeof change: ', typeof change);
+
+var quizzes = 83;
+var midterm = 97;
+var final = 90;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade: ', grade);
+console.log('typeof grade: ', typeof grade);
+
+var firstName = 'Jared';
+var lastName = 'Jenvey';
+var fullName = `${firstName} ${lastName}`;
+console.log('value of fullName: ', fullName);
+console.log('typeof fullName: ', typeof fullName);
+
+var pH = 6;
+var isAcidic = pH < 7;
+console.log('value of isAcidic: ', isAcidic);
+console.log('typeof isAcidic: ', typeof isAcidic);
+
+var headCount = 34;
+var isSparta = headCount === 300;
+console.log('value of isSparta: ', isSparta);
+console.log('typeof isSparta: ', typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('value of motto: ', motto);
+console.log('typeof motto: ', typeof motto);
