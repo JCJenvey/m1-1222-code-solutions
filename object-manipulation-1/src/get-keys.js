ff24f9fd11df84e49e5f1717cb3300c93607d03a
Jared Jenvey <jared@exit-12.com>
2023-01-11 04:39:27 +0000

    moved variable declarations and removed unneeded return statements in get-keys.js and get-values.js

--- object-manipulation-1/src/get-keys.js
+++ object-manipulation-1/src/get-keys.js
@@ -1,23 +1,21 @@
 /* exported getKeys */
 
 // define a function named getKeys with 1 argument; object
+//   declare a variable and assign it to an empty array
 //   check if object is an object, and NOT an array or null
 //     if true:
-//     declare a variable and assign it to an empty array
 //     begin a loop that iterates through the keys in the 'object' object, where
 //       for each key in the object:
 //         add the key to the end of the new array
-//       return the new array
-//   return an empty array
+//     return the new array
 
 function getKeys(object) {
+  var keys = [];
   if (typeof object === 'object' && !Array.isArray(object) &&
     object !== null) {
-    var keys = [];
     for (var key in object) {
       keys.push(key);
     }
-    return keys;
   }
-  return [];
+  return keys;
 }
--- object-manipulation-1/src/get-values.js
+++ object-manipulation-1/src/get-values.js
@@ -1,23 +1,21 @@
 /* exported getValues */
 
 // define a function named getValues with 1 argument; object
+//   declare a variable and assign it to an empty array
 //   check if object is an object, and NOT an array or null
 //     if true:
-//     declare a variable and assign it to an empty array
 //     begin a loop that iterates through the keys in the 'object' object, where
 //       for each key in the object:
 //         add the value of the key to the end of the new array
-//       return the new array
-//   return an empty array
+//     return the new array
 
 function getValues(object) {
+  var values = [];
   if (typeof object === 'object' && !Array.isArray(object) &&
     object !== null) {
-    var values = [];
     for (var key in object) {
       values.push(object[key]);
     }
-    return values;
   }
-  return [];
+  return values;
 }
